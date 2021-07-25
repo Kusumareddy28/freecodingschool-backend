2f44586c38fd074ec12395457a383ab0d0d50cd0
premnair34 <premnair.r@gmail.com>
2021-07-25 23:52:16 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,10 +5,7 @@ exports.init = (router) => {
     router.post('/api/user',user.signup);
     router.post('/api/user/auth', user.signin);
     router.get('/api/user',user.getUser);
-<<<<<<< HEAD
-=======
     router.post('/api/admin/courses',courses.course);
     router.post('/api/user/testimonial',testimonial.review);
->>>>>>> test
 }
 
